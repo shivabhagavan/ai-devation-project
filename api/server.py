5a918260700477555591b5281adae6f3f5290cc3
Shivabhagavan <avihsntr@gmail.com>
2026-05-08 16:16:11 +0530

    fix startup path for azure

--- api/server.py
+++ api/server.py
@@ -688,4 +688,4 @@ def create_deviation_form(form_data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api.server:app", host="0.0.0.0", port=8000, reload=True)
